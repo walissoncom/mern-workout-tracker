8c6acdba5f8c1bfbea1c649ccb53b967e4fa2734
Walisson Rodrigues <contact@walisson.com>
2020-12-19 03:09:21 +1100

    WorkoutEdit component implementation + API edit method implemented

--- src/components/WorkoutList/WorkoutList.js
+++ src/components/WorkoutList/WorkoutList.js
@@ -9,6 +9,7 @@ const Workout = props => (
         <td>{props.workout.duration}</td>
         <td>{props.workout.date.substring(0, 10)}</td>
         <td>
+            <Link to={'/edit/' + props.workout._id}>Edit</Link> |
             <a href="#" onClick={() => props.deleteWorkout(props.workout._id)}>Delete</a>
         </td>
     </tr>
