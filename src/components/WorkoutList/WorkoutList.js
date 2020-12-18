192d131959a8f125361a6a881c2a2c4de77f2f8f
Walisson Rodrigues <contact@walisson.com>
2020-12-19 02:06:18 +1100

    WorkoutList component - API get method implemented

--- src/components/WorkoutList/WorkoutList.js
+++ src/components/WorkoutList/WorkoutList.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class WorkoutList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.deleteWorkout = this.deleteWorkout.bind(this);
+
+        this.state = { workouts: [] }
+    }
+
+    componentDidMount() {
+
+        axios.get('http://localhost:5000/workouts/')
+            .then(response => {
+
+                if (response.data.length > 0) {
+                    this.setState({ workouts: response.data })
+                }
+            })
+            .catch(err => console.error(err))
+    }
+
+    workoutList() {
+        return this.state.workouts.map(currentWorkout => {
+            return (
+                <div>
+                    <span>{currentWorkout.username} | </span>
+                    <span>{currentWorkout.description} | </span>
+                    <span>{currentWorkout.duration} | </span>
+                    <span>{currentWorkout.date}</span>
+                </div>
+            )
+        })
+    }
+
     render() {
         return (
             <div>
-                <p>You are on the Workout List component!</p>
+                <h3>Logged Workouts</h3>
+                {this.workoutList()}
             </div>
         )
     }
