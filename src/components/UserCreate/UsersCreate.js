dfac10e47f7f211f764a64141421fbfd5deebc53
Walisson Rodrigues <contact@walisson.com>
2020-12-19 00:47:07 +1100

    User Create component - HTML form creation + Event handling + State update

--- src/components/UserCreate/UsersCreate.js
+++ src/components/UserCreate/UsersCreate.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
 
 export default class UserCreate extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.userInput = React.createRef();
+
+        this.state = {
+            username: ''
+        }
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username
+        }
+
+        console.log(user);
+
+        this.setState({
+            username: ''
+        })
+    }
+
     render() {
         return (
             <div>
-                <p>You are on the User Create component!</p>
+                <h3>Create User</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username:</label>
+                        <input
+                            ref={this.userInput}
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername} />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary">Create User</button>
+                    </div>
+                </form>
             </div>
         )
     }
